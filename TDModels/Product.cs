e77fecd55f4dc4d436c900ae2e8a90835ebe5a1a
Ing. Germán Andrés Olaya Acuña <german.olaya.a@outlook.com>
2022-05-01 23:30:16 -0500

    Estructura: Modelos, Vistas y Presentadores

---
+++ TDModels/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace TDModels
+{
+    public class Product
+    {
+        // Product Data Fields
+        private int id;
+        private string code;
+        private string name;
+        private int price;
+
+
+    }
+}
